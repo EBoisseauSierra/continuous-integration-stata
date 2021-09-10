1ff94972b89ef6a297d8a6ef0eca21eeccd5914d
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2021-09-10 12:28:46 +0100

    Use docker image with Stata pre-installed installed
    Signed-off-by: Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
 # Container image that runs your code
-FROM ubuntu:18.04
-RUN apt-get update
-RUN apt-get upgrade -y
-RUN apt-get clean -y
-RUN apt-get install wget -y
-RUN apt-get install texlive-latex-base -y
-RUN wget http://security.ubuntu.com/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1_amd64.deb
-RUN dpkg -i libpng12-0_1.2.54-1ubuntu1.1_amd64.deb
+FROM eboisseausierra/stata:0.0.2
+
+COPY stata.lic /usr/local/stata/stata.lic
+RUN ["chmod", "a+r", "/usr/local/stata/stata.lic"]
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,46 +1,9 @@
 #!/bin/bash
 
-# install stata
-mkdir ~/Downloads
-cd ~/Downloads
-wget -O Stata$2Linux64.tar.gz $1
-cd /tmp/
-mkdir statafiles
-cd statafiles
-tar -zxf ~/Downloads/Stata$2Linux64.tar.gz
-cd /usr/local
-mkdir stata$2
-cd stata$2
-yes | /tmp/statafiles/install
-export PATH=/usr/local/stata$2:$PATH
-./stinit << EOF
-y
-y
-$3
-$4
-$5
-y
-y
-$6
-$7
-y
-EOF
+# populate licence file
+echo -n "$1" > /usr/local/stata/stata.lic
 
-# back to the default working directory
-cd /github/workspace
-
-# run do-file
-echo $8
-if [ $8 == "stata" ]; then
-    echo $8
-    stata -b do main
-elif [ $8 == "stata-se" ]; then
-    echo $8
-    stata-se -b do main
-elif [ $8 == "stata-mp" ]; then
-    echo $8
-    stata-mp -b do main
-fi
+$2 -b do main
 
 # print log result
 cat main.log
