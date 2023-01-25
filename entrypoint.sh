6cd7fe29c8955d37941934729972057626092269
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2023-01-25 11:32:24 +0000

    ci: Don't generate artefacts summary here
    Signed-off-by: Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,19 +14,3 @@ EXIT_CODE=$(tail -1 main.log | tr -d '[:cntrl:]')
 if [[ ${EXIT_CODE:0:1} == "r" ]]; then
     exit 1
 fi
-
-# Source: how to comment on multi-line commands https://stackoverflow.com/a/12797512/5433628
-echo "ARTEFACTS_SUMMARY<<EOF" >> $GITHUB_OUTPUT
-find results/ -type f -exec sha512sum {} \; | sed 's/  /,/' | cat <(echo 'sha512,file_name') - >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
-
-
-# # Add the list of artefacts and their SHA512 sum as a 'git notes' to HEAD
-# git notes remove --ignore-missing #  Clear note if already existing
-
-# # Source: how to comment on multi-line commands https://stackoverflow.com/a/12797512/5433628
-# find results/ -type f `# list all artefacts…` \
-#     -exec sha512sum {} \; `# … and compute their SHA-512 sum: https://askubuntu.com/a/1091369`\
-#     | sed 's/  /,/' `# replace the double-space separator between sha and filename with a comma`\
-#     | cat <(echo "sha512,file_name") - `# prefix the git note on the fly, as appending creates new line https://stackoverflow.com/a/58287466/5433628 + https://stackoverflow.com/a/33139133/5433628`\
-#     | git -c "user.name=Github Actions" -c "user.email=bot@github.actions" notes add --allow-empty -F -
